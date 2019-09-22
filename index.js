3c8f4cb9e259e24a6ed315398c57dc7a7bec29b9
Utsav <trivediutsav27@gmail>
2019-09-22 14:49:52 -0400

    index.js

--- index.js
+++ index.js
@@ -1,39 +1,52 @@
-class org {
-    constructor(name){
-        this.name = name;
+class Org {
+    constructor(students){
+        this.students = students;
     }
-    student(){
-        console.log(`${this.name}`);
-    }
-
-    dept(){
-        console.log(`${this.name}`);
-    }
-}
-class dept extends org{
-    constructor(name){
-        this.name = name;
-    }
-    maths(){
-        console.log(`${this.name}`);
-    }
-
-    bio(){
-        console.log(`${this.name}`);
+    
+    displayStudents(){
+        for(let i=0;i<this.students.length;i++){
+            console.log(this.students[i].name);
+        }
     }
 }
 
+class Dept{
 
-class student extends dept {
-    constructor(name){
+    constructor(students){
+        this.students = students;
+    }
+    
+    putTheStudentInDept(){
+        var itdept = [];
+        var csdept = [];
+        var students = [itdept,csdept];
+
+        for(let i=0;i<this.students.length;i++){
+            if(this.students[i].dept === 'IT'){
+                itdept.push(this.students[i]);
+            }else {
+                csdept.push(this.students[i]);            }
+        }
+
+        return students;
+    }
+
+}
+
+
+class Student {
+    constructor(id,name,dept,org){
+        this.id = id;
         this.name = name;
+        this.dept = dept;
+        this.org = org;
     }
 
     name(){
         console.log(`${this.name}`);
     }
 
-    rollno(){
+    id(){
         console.log(`${this.name}`);
     }
 
@@ -46,4 +59,21 @@ class student extends dept {
     }
 }
 
+var s1 = new Student(1,'Jaye','CS','GS');
+var s2 = new Student(2,'Utsav','IT','GS');
+var s3 = new Student(3,'Sam','CS','GS');
 
+var students = [s1,s2,s3];
+
+var tempDept = new Dept(students);
+
+var returnedStudents = tempDept.putTheStudentInDept();
+
+console.log(returnedStudents);
+
+var tempOrg = new Org(students);
+
+console.log('===================================================');
+
+
+tempOrg.displayStudents();
